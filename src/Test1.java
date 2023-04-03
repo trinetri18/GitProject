4eacce5330712378ab86ff78bb80ade4292641d6
HP <HP@DESKTOP-Q4SL2B3>
2023-04-03 16:16:36 +0530

    first commit

---
+++ src/Test1.java
@@ -0,0 +1,17 @@
+
+public class Test1 {
+
+	public static void main(String ar[])
+	{
+		String st1="Yes";
+		if (st1.equals("Yes"))
+		{
+			System.out.println("Ok...Do something");
+		}
+		else
+		{
+			System.out.println("do nothing");
+		}
+	}
+	
+}
